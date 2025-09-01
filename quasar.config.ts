3b57b5edbf36d5648b69d28d666117ea3ba67127
PS-LostInSpace <per.salander@combitech.com>
2025-09-01 13:00:14 +0200

    Initierar Quasar/Vue-projekt och installerar beroenden

--- quasar.config.ts
+++ quasar.config.ts
@@ -50,7 +50,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-       //publicPath: '/repo namn',
+      publicPath: '/PS-LostInSpace/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,20 @@
 <template>
   <q-page class="row items-center justify-center">
-    <q-card class="q-pa-md text-center">
-      <q-card-section>
-        <!-- Här ska vi fylla i våra komponenter -->
+    <q-card class="q-pa-lg q-mx-auto q-my-xl text-center" style="max-width: 500px">
+      <q-card-section class="column items-center q-gutter-md">
+        <ExampleComponent
+          title="Hello to Moas and Pers World"
+          subtitle="Cavatech Tutorial"
+          subsubtitle="tutorial"
+          active
+        />
+        <ButtonComponent buttontext="Click here" link="https://quasar.dev/introduction-to-quasar" />
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
-// import statements
+import ExampleComponent from 'components/ExampleComponent.vue';
+import ButtonComponent from 'components/ButtonComponent.vue';
 </script>
